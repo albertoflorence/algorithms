c6c793a1b3bba66f3940ddbc81c252afa7ac264d
albertoflorence <alberto.sabatier@gmail.com>
2023-11-29 16:13:31 -0300

    requisito 5 - find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    table = set()
+    for n in nums:
+        if not isinstance(n, int) or n < 0:
+            return False
+        if n in table:
+            return n
+        else:
+            table.add(n)
+
+    return False
