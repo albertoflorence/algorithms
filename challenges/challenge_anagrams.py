486dedbcc9b7b1c4a7637dc8b5a8547414f6b5d0
albertoflorence <alberto.sabatier@gmail.com>
2023-11-29 15:52:51 -0300

    requisito 4 - anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,25 @@
+def sort(arr, callback):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+    for i in range(1, len(arr)):
+        item = arr[i]
+        if callback(item, pivot):
+            left.append(item)
+        else:
+            right.append(item)
+
+    return sort(left, callback) + [pivot] + sort(right, callback)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_list = sort(first_string, lambda a, b: a.lower() < b.lower())
+    second_list = sort(second_string, lambda a, b: a.lower() < b.lower())
+
+    first = "".join(first_list).lower()
+    second = "".join(second_list).lower()
+
+    return (first, second, first == second and first != "")
