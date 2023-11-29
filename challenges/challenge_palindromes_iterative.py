f04600969b2cc5004fe85fc36f961bbdcea8c28a
albertoflorence <alberto.sabatier@gmail.com>
2023-11-29 16:18:59 -0300

    requisito 6 - palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    size = len(word)
+    mid = size // 2
+    for i in range(0, mid):
+        if word[i] != word[size - 1 - i]:
+            return False
+    return True
